f091ccde204813d0c1933c8af1555ed99dfed16c
mindfog <mindfog>
2016-03-17 03:32:32 +0200

    Assembly name fix.

--- TabNavigation/Properties/AssemblyInfo.cs
+++ TabNavigation/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TabGroupSwitch")]
+[assembly: AssemblyTitle("TabNavigation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TabGroupSwitch")]
+[assembly: AssemblyProduct("TabNavigation")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
